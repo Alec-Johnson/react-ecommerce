6f19813e9280175d9c6beef914ca8cd08b63e9d4
Alec Johnson <akj3211@gmail.com>
2021-05-13 19:14:17 -0500

    redux functionality for products

---
+++ src/redux/Products/products.sagas.js
@@ -0,0 +1,67 @@
+import { auth } from './../../firebase/utils'
+import { put, call, all, takeLatest } from 'redux-saga/effects'
+import {
+  handleAddProduct,
+  handleFetchProducts,
+  handleDeleteProduct,
+} from './products.helpers'
+import productsTypes from './products.types'
+import { setProducts, fetchProductsStart } from './products.actions'
+
+// Add product to store
+export function* addProduct({
+  payload: { productCategory, productName, productPrice, productThumbnail },
+}) {
+  try {
+    const timestamp = new Date()
+    yield handleAddProduct({
+      productCategory,
+      productName,
+      productPrice,
+      productThumbnail,
+      productAdminUserUID: auth.currentUser.uid,
+      createdAt: timestamp,
+    })
+    yield put(fetchProductsStart())
+  } catch (err) {
+    console.log(err)
+  }
+}
+
+export function* onAddProductStart() {
+  yield takeLatest(productsTypes.ADD_NEW_PRODUCT_START, addProduct)
+}
+
+export function* fetchProducts() {
+  try {
+    const products = yield handleFetchProducts()
+    yield put(setProducts(products))
+  } catch (err) {
+    console.log(err)
+  }
+}
+
+export function* onFetchProductsStart() {
+  yield takeLatest(productsTypes.FETCH_PRODUCTS_START, fetchProducts)
+}
+
+export function* deleteProduct({ payload }) {
+  try {
+    yield handleDeleteProduct(payload)
+    yield put(fetchProductsStart())
+  } catch (err) {
+    console.log(err)
+  }
+}
+
+export function* onDeleteProductStart() {
+  yield takeLatest(productsTypes.DELETE_PRODUCT_START, deleteProduct)
+}
+
+export default function* productsSagas() {
+  yield all([
+    call(onAddProductStart),
+    call(onFetchProductsStart),
+    call(onDeleteProductStart),
+  ])
+}
