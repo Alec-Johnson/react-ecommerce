1fef63e46b0aee57117040d6104bdac7e663b687
Alec Johnson <akj3211@gmail.com>
2021-05-18 16:24:25 -0500

    apiInstance for firebase

--- src/utils/index.js
+++ src/utils/index.js
@@ -1,8 +1,14 @@
+import axios from "axios";
+
 export const checkUserIsAdmin = (currentUser) => {
-  if (!currentUser || !Array.isArray(currentUser.userRoles)) return false // no access
+  if (!currentUser || !Array.isArray(currentUser.userRoles)) return false; // no access
 
-  const { userRoles } = currentUser
-  if (userRoles.includes('admin')) return true // admin access
+  const { userRoles } = currentUser;
+  if (userRoles.includes("admin")) return true; // admin access
 
-  return false
-}
+  return false;
+};
+
+export const apiInstance = axios.create({
+  baseURL: "http://localhost:5001/react-ecommerce-e6547/us-central1/api", // firebase function api url
+});
