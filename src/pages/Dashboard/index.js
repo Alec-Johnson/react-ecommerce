c6cc63c6e075102adb4366813f910413adc7e1eb
Alec Johnson <akj3211@gmail.com>
2021-05-18 16:37:27 -0500

    Allow user to view their previous orders

--- src/pages/Dashboard/index.js
+++ src/pages/Dashboard/index.js
@@ -1,7 +1,27 @@
-import React from 'react'
+import React, { useEffect } from "react";
+import { useDispatch, useSelector } from "react-redux";
+import { getUserOrderHistory } from "./../../redux/Orders/orders.actions";
+import OrderHistory from "./../../components/OrderHistory";
+const mapState = ({ user, ordersData }) => ({
+  currentUser: user.currentUser,
+  orderHistory: ordersData.orderHistory.data,
+});
 
-const Dashboard = (props) => {
-  return <h1>Logged in</h1>
-}
+const Dashboard = () => {
+  const dispatch = useDispatch();
+  const { currentUser, orderHistory } = useSelector(mapState);
 
-export default Dashboard
+  useEffect(() => {
+    dispatch(getUserOrderHistory(currentUser.id));
+  }, []);
+
+  return (
+    <div>
+      <h1>Order History</h1>
+
+      <OrderHistory orders={orderHistory} />
+    </div>
+  );
+};
+
+export default Dashboard;
